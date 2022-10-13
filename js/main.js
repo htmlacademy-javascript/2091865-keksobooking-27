7e026a6bf9f1f3bff8ab17150b44f9d5feed3ce1
Алина <alinarom21@mail.ru>
2022-10-14 00:08:32 +0300

    вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function getRandomPositiveInteger (a, b) {
+  if(typeof a !== 'number' || typeof b !== 'number') {
+    return NaN;
+  }
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a, b)); // округление в меньшую //
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result); // округление в нижнюю сторону
+}
+
+function getRandomPositiveFloat (a, b, digits = 1) {
+  if(typeof a !== 'number' || typeof b !== 'number') {
+    return NaN;
+  }
+  if (a < 0 || b < 0 || digits < 0) {
+    return NaN;
+  }
+  const lower = Math.min(a, b);
+  const upper = Math.max (a, b);
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
+}
+
+getRandomPositiveInteger (1, 0);
+getRandomPositiveFloat (0, 1, 2);
