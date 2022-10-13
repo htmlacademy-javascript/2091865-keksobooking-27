f81122369c2266e7afd842b4e0dc923d00d3007e
Алина <alinarom21@mail.ru>
2022-10-14 00:25:25 +0300

    изменения

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Функции взяты из интернета и доработана
+// Функции взяты из интернета и доработаны
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 
 function getRandomPositiveInteger (a, b) {
@@ -8,7 +8,7 @@ function getRandomPositiveInteger (a, b) {
   if (a < 0 || b < 0) {
     return NaN;
   }
-  const lower = Math.ceil(Math.min(a, b)); // округление в меньшую //
+  const lower = Math.ceil(Math.min(a, b)); // округление в меньшую
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result); // округление в нижнюю сторону
